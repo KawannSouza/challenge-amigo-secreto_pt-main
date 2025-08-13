70ee0916e6c866a8ff4095b990d4c7cce7c09f4f
Kawann <kawannmarcelo7@gmail.com>
2025-08-12 21:21:36 -0300

    criando função de adicionar um amigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    let res = document.getElementById("listaAmigos");
+
+    if (!nomeAmigo) {
+        alert("O campo de nome não pode estar vazio!")
+    }else {
+        listaAmigos.push(nomeAmigo);
+
+        res.innerHTML += `<li>${nomeAmigo}</li>`;
+    }
+}
